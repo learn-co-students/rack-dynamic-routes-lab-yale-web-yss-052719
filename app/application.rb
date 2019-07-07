c5800c83b85c1072ada76182e8b6bf74a885af3a
MatthewP01 <matthew.peoples@yale.edu>
2019-07-06 22:17:55 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+    def call(x)
+        resp = Rack::Response.new
+        req = Rack::Request.new(x)
+
+        if req.path.match("/items/")
+            iname = req.path.split("/items/").last
+            item = @@items.find do |item|
+                item.name == iname
+            end
+            if item != nil
+                resp.write item.price
+                resp.status = 200
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+
+        resp.finish
+    end
+end
